e12a5563a9bee2253b8b43c001cc189ad055bb46
panukettu <panu.t.kettunen@gmail.com>
2019-01-21 21:20:10 +0200

    added a header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 
+import HeaderNav from "./containers/HeaderNav/HeaderNav";
+
 class App extends Component {
   render() {
-    return <div>Wow</div>;
+    return (
+      <div>
+        <HeaderNav />
+        Wow
+      </div>
+    );
   }
 }
 
